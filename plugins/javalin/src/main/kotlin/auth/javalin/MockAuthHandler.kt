a5301fa5ef17c89f05aba03ba9dd687845b6dacb
Tom Warner <tomwamt@gmail.com>
2025-03-26 09:09:49 -0600

    WIP proxy handler

--- plugins/javalin/src/main/kotlin/auth/javalin/MockAuthHandler.kt
+++ plugins/javalin/src/main/kotlin/auth/javalin/MockAuthHandler.kt
@@ -4,6 +4,10 @@ import auth.api.v1.*
 import io.javalin.Javalin
 import io.javalin.http.NotFoundResponse
 import multiplatform.javalin.handleApi
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.util.*
 
 class MockAuthHandler {
@@ -25,5 +29,30 @@ class MockAuthHandler {
             .handleApi(register) { AuthenticatedUser(mockUserId, mockToken) }
             .handleApi(signIn) { AuthenticatedUser(mockUserId, mockToken) }
             .handleApi(getGoogleClientId) { throw NotFoundResponse() }
+    }
+}
+
+class ProxyAuthHandler(private val baseUrl: String) {
+    private val client = HttpClient.newHttpClient()
+
+    fun registerRoutes(app: Javalin) {
+        app.before("/auth/*") { ctx ->
+            val pathWithQuery = ctx.path() + ctx.queryString()?.let { "?$it" }.orEmpty()
+            val req = HttpRequest.newBuilder(URI("http://$baseUrl$pathWithQuery"))
+            val body = if (ctx.contentLength() > 0) {
+                HttpRequest.BodyPublishers.ofByteArray(ctx.bodyAsBytes())
+            } else {
+                HttpRequest.BodyPublishers.noBody()
+            }
+            req.method(ctx.method().name, body)
+            val authHeader = ctx.header("Authorization")
+            if (authHeader != null) {
+                req.header("Authorization", authHeader)
+            }
+
+            val resp = client.send(req.build(), HttpResponse.BodyHandlers.ofInputStream())
+
+            ctx.status(resp.statusCode()).result(resp.body()).skipRemainingHandlers()
+        }
     }
 }
